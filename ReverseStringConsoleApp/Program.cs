d596dc4630c0abedfeb3e4a2697d208e61d0c6e5
rkbhowmik <rajib.bhowmik@gmail.com>
2017-11-01 23:12:51 +0100

    Add project files.

---
+++ ReverseStringConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseStringConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("******** A program to reverse an input string ********\n\n ");
+
+			Console.Write("Enter your word: ");
+			var input = Console.ReadLine();
+			char[] inputarr = input.ToCharArray();
+			Array.Reverse(inputarr);
+			Console.WriteLine("Reversed order: " + new String(inputarr)); 
+			Console.ReadLine();
+		}
+	}
+}
